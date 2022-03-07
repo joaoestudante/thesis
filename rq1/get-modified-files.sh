b049688e2854bfeb5f1c173b0994a99c99b06840
João Lourenço <estudante.lourenco@gmail.com>
2022-03-07 15:12:15 +0000

    Delete unused script

--- rq1/get-modified-files.sh
+++
@@ -1,3 +0,0 @@
-for x in $(cd ../codebases/fenixedu-academic && git log --pretty=format:"%h");
-	do echo $(cd ../codebases/fenixedu-academic && git diff-tree --no-commit-id --name-only -r $x);
-done
