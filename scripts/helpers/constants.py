c51a0e90e792e598b3678ef3c49d2d67e858d2d1
João Lourenço <estudante.lourenco@gmail.com>
2022-07-25 12:02:43 +0100

    Add csv with codebases data

--- scripts/helpers/constants.py
+++ scripts/helpers/constants.py
@@ -7,5 +7,6 @@ class Constants:
     group_commits_interval: int = 3600
     project_root: Path = Path(__file__).parent.parent
     codebases_data_output_directory: str = str(project_root) + "/resources/codebases_collection"
+    resources_directory: str = str(project_root) + "/resources"
     codebases_root_directory: str = str(project_root.parent) + "/codebases"
     mono2micro_codebases_root: str = str(project_root.parent) + "/mono2micro-mine/codebases"
